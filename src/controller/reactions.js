00ed7e6c6f4e8eab3d3257882d87055531f8a401
Michelle <michellephant@pm.me>
2022-03-22 18:31:15 +0100

    if user is not admin return

--- src/controller/reactions.js
+++ src/controller/reactions.js
@@ -1,11 +1,9 @@
-import fs from 'fs';
-// require the yaml module npm i yaml
-import YAML from 'yaml';
-
-const messageConfig = YAML.parse(fs.readFileSync('./config/messagesWithButtons.yml', 'utf8'));
-
 export const botAddsReaction = (app) => {
-  app.event('reaction_added', async ({ event, client }) => {
-    console.log('yay');
+  app.event('reaction_added', async ({ event, client, message, say, body }) => {
+    if (event.user === 'U035UDUM0H0') return;
+    if (event.reaction === 'white_check_mark') {
+      console.log('that worked');
+      console.log(event);
+    }
   });
 };
