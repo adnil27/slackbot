9115e1fa17a2d4120d3ba087243fa82f85172998
Michelle <michellephant@pm.me>
2022-03-22 22:45:56 +0100

    reaction removed, removes bot emoji

--- src/controller/reactions.js
+++ src/controller/reactions.js
@@ -1,8 +1,24 @@
 export const botAddsReaction = (app) => {
-  app.event('reaction_added', async ({ event, context }) => {
+  app.event('reaction_added', async ({ event, context, body, message }) => {
     if (event.user === 'U035UDUM0H0') return;
     if (event.reaction === 'white_check_mark' && event.item.channel === 'C034H2X55D4') {
-      console.log('that worked');
+      console.log(event);
+      app.client.reactions.add({
+        name: 'robot_face',
+        timestamp: event.item.ts,
+        channel: event.item.channel
+      });
+    }
+  });
+  app.event('reaction_removed', async ({ event, context, body, message }) => {
+    if (event.user === 'U035UDUM0H0') return;
+    if (event.reaction === 'white_check_mark' && event.item.channel === 'C034H2X55D4') {
+      console.log(event);
+      app.client.reactions.remove({
+        name: 'robot_face',
+        timestamp: event.item.ts,
+        channel: event.item.channel
+      });
     }
   });
 };
