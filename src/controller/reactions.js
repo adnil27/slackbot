e4bbdd0461a99aab51a7ace8a50e1368ce6e4598
Michelle <michellephant@pm.me>
2022-03-22 22:22:05 +0100

    reaction added in ps bot only

--- src/controller/reactions.js
+++ src/controller/reactions.js
@@ -1,7 +1,7 @@
 export const botAddsReaction = (app) => {
-  app.event('reaction_added', async ({ event, client, message, say, body }) => {
+  app.event('reaction_added', async ({ event, context }) => {
     if (event.user === 'U035UDUM0H0') return;
-    if (event.reaction === 'white_check_mark') {
+    if (event.reaction === 'white_check_mark' && event.item.channel === 'C034H2X55D4') {
       console.log('that worked');
     }
   });
