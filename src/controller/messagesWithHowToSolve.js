cba9d5298914c084612dd8f38217e6267b99da24
Michelle <michellephant@pm.me>
2022-03-31 18:06:42 +0200

    postReplyWithResolution

--- src/controller/messagesWithHowToSolve.js
+++ src/controller/messagesWithHowToSolve.js
@@ -2,64 +2,7 @@ import { logger } from '../utils/logger.js';
 
 import { messageConfig } from './messageConfig.js';
 
-const postReply = (app, message, context, introduction, solution, greeting, extraInformation, isSolved) => {
-  try {
-    app.client.chat.postMessage({
-      token: context.botToken,
-      channel: message.channel,
-      user: message.user,
-      thread_ts: message.ts,
-      text: ' ',
-      blocks: [
-        {
-          type: 'divider'
-        },
-        {
-          type: 'section',
-          text: {
-            type: 'mrkdwn',
-            text: greeting + `<@${message.user}>` + introduction
-          }
-        },
-        {
-          type: 'divider'
-        },
-        {
-          type: 'section',
-          text: {
-            type: 'mrkdwn',
-            text: solution
-          }
-        },
-        {
-          type: 'divider'
-        },
-        {
-          type: 'section',
-          text: {
-            type: 'mrkdwn',
-            text: extraInformation
-          }
-        },
-        {
-          type: 'divider'
-        },
-        {
-          type: 'section',
-          text: {
-            type: 'mrkdwn',
-            text: isSolved
-          }
-        },
-        {
-          type: 'divider'
-        }
-      ]
-    });
-  } catch (e) {
-    console.log(e);
-  }
-};
+import { postReplyWithResolution } from './postReplyWithResolution.js';
 
 export const messageWithReactions = (app) => {
   for (const res of messageConfig.replies) {
@@ -69,6 +12,7 @@ export const messageWithReactions = (app) => {
     app.message(caseCheckMessage, ({ message, context }) => {
       let ignoreMessage = false;
       if (message.user === res.psTeamUsers) return;
+      if (!message.text.match(caseCheckMessage)) return;
       if (!message.text.match(caseCheckAction)) return;
       if (message.channel !== res.onlyChannel) return;
       if (res.ignoreIfContains) {
@@ -77,9 +21,8 @@ export const messageWithReactions = (app) => {
           if (regex.test(message.text)) ignoreMessage = true;
         }
       }
-      console.log('hello');
       !ignoreMessage
-        ? postReply(app, message, context, res.introduction, res.solution, res.greeting, res.extraInformation, res.isSolved)
+        ? postReplyWithResolution(app, message, context, res.introduction, res.solution, res.greeting, res.extraInformation, res.isSolved)
         : logger('info', 'This message was ignored as it matched an ignoreIfContains regex test');
     });
   }
---
+++ src/controller/postReplyWithResolution.js
@@ -0,0 +1,58 @@
+export const postReplyWithResolution = (app, message, context, introduction, solution, greeting, extraInformation, isSolved) => {
+  try {
+    app.client.chat.postMessage({
+      token: context.botToken,
+      channel: message.channel,
+      user: message.user,
+      thread_ts: message.ts,
+      text: ' ',
+      blocks: [
+        {
+          type: 'divider'
+        },
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: greeting + `<@${message.user}>` + introduction
+          }
+        },
+        {
+          type: 'divider'
+        },
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: solution
+          }
+        },
+        {
+          type: 'divider'
+        },
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: extraInformation
+          }
+        },
+        {
+          type: 'divider'
+        },
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: isSolved
+          }
+        },
+        {
+          type: 'divider'
+        }
+      ]
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
