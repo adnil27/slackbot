ff0336fb2e90762b962b7cd8437ae3f8248e71cf
Michelle <michellephant@pm.me>
2022-04-04 23:41:00 +0200

    postresolution tweaks

--- src/controller/messagesWithHowToSolve.js
+++ src/controller/messagesWithHowToSolve.js
@@ -1,5 +1,3 @@
-import { logger } from '../utils/logger.js';
-
 import { messageConfig } from './messageConfig.js';
 
 import { postReplyWithResolution } from './postReplyWithResolution.js';
@@ -7,15 +5,15 @@ import { postReplyWithResolution } from './postReplyWithResolution.js';
 export const messageWithReactions = (app) => {
   for (const res of messageConfig.replies) {
     const matchMessage = new RegExp(res.message, 'i');
-    const matchAction = new RegExp(res.ifContains, 'i');
     const matchIgnoreIfContains = new RegExp(res.ignoreIfContains, 'i');
+    const matchIfContains = new RegExp(res.ifContains, 'i');
 
     app.message(matchMessage, ({ message, context }) => {
       if (message.user === res.psTeamUsers) return;
       if (message.channel !== res.onlyChannel) return;
-      if (!message.text.match(matchAction)) return;
-      if (!message.text.match(matchIgnoreIfContains)) {
-        postReplyWithResolution(app, message, context, res.introduction, res.solution, res.greeting, res.extraInformation, res.isSolved);
+      if (message.text.match(matchIgnoreIfContains)) return;
+      if (message.text.match(matchIfContains)) {
+        postReplyWithResolution(app, message, context, res.reply, res.introduction, res.solution, res.extraInformation, res.isSolved);
       }
     });
   }
