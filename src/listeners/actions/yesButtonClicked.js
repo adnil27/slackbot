c2af9b518cf6f7602c4d4724c700202dc87df52c
Michelle <michellephant@pm.me>
2022-04-07 21:06:14 +0200

    fixed action ts

--- src/listeners/actions/yesButtonClicked.js
+++ src/listeners/actions/yesButtonClicked.js
@@ -1,24 +1,24 @@
 export const actionController = (app) => {
-  app.action('yes_button', async ({ ack, action, body, client }) => {
+  app.action('yes_button', async ({ ack, action, body, context, client }) => {
     await ack();
+    console.log(action);
+    if (body.message) {
+      const result = await app.client.conversations.history({
+        channel: body.channel.id,
+        latest: body.message.ts,
+        inclusive: true,
+        limit: 1
+      });
 
-    const messageID = action.ts;
-    const channelID = body.channel.id;
+      const messageResult = result.messages[0];
+      console.log(messageResult);
 
-    const result = await app.client.conversations.history({
-      channel: channelID,
-      latest: messageID,
-      inclusive: true,
-      limit: 1
-    });
-
-    const messageResult = result.messages[0];
-    console.log(messageResult);
-
-    await app.client.reactions.add({
-      name: 'white_check_mark',
-      timestamp: messageResult.ts,
-      channel: body.channel.id
-    });
+      const result1 = await client.reactions.add({
+        name: 'white_check_mark',
+        timestamp: messageResult.ts,
+        channel: body.channel.id
+      });
+      console.log(result1);
+    }
   });
 };
